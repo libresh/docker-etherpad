5d43ffc2a85554927708d7846dccfe8dd18fa5e3
Pierre Ozoux <pierre@ozoux.net>
2016-07-13 14:22:21 +0100

    simplify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ -z "$MYSQL_PORT_3306_TCP_ADDR" ]; then
-  echo >&2 'error: missing MYSQL_PORT_3306_TCP environment variable'
-  echo >&2 '  Did you forget to --link some_mysql_container:mysql ?'
-  exit 1
-fi
-
 # if we're linked to MySQL, and we're using the root user, and our linked
 # container has a default "root" password set up and passed through... :)
 : ${ETHERPAD_DB_USER:=root}
 if [ "$ETHERPAD_DB_USER" = 'root' ]; then
-  : ${ETHERPAD_DB_PASSWORD:=$MYSQL_ENV_MYSQL_ROOT_PASSWORD}
+  : ${ETHERPAD_DB_PASSWORD:=$DB_ENV_MYSQL_ROOT_PASSWORD}
 fi
 : ${ETHERPAD_DB_NAME:=etherpad}
 
@@ -30,34 +24,44 @@ fi
 : ${ETHERPAD_SESSION_KEY:=$(
     node -p "require('crypto').randomBytes(32).toString('hex')")}
 
+for ((i=0;i<10;i++))
+do
+  DB_CONNECTABLE=$(mysql -uroot -p${ETHERPAD_DB_PASSWORD} -hdb -e 'status' >/dev/null 2>&1; echo "$?")
+  if [[ DB_CONNECTABLE -eq 0 ]]; then
+    echo "connected to mysql"
+    break
+  fi
+  sleep 5
+done
+
 # Check if database already exists
 RESULT=`mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} \
-  -hmysql --skip-column-names \
+  -hdb --skip-column-names \
   -e "SHOW DATABASES LIKE '${ETHERPAD_DB_NAME}'"`
 
 if [ "$RESULT" != $ETHERPAD_DB_NAME ]; then
   # mysql database does not exist, create it
   echo "Creating database ${ETHERPAD_DB_NAME}"
 
-  mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} -hmysql \
+  mysql -u${ETHERPAD_DB_USER} -p${ETHERPAD_DB_PASSWORD} -hdb \
         -e "create database ${ETHERPAD_DB_NAME}"
 fi
 
 if [ ! -f settings.json ]; then
 
-cat <<- EOF > settings.json
-  {
-    "title": "${ETHERPAD_TITLE}",
-    "ip": "0.0.0.0",
-    "port" :${ETHERPAD_PORT},
-    "sessionKey" : "${ETHERPAD_SESSION_KEY}",
-    "dbType" : "mysql",
-    "dbSettings" : {
-          "user"    : "${ETHERPAD_DB_USER}",
-          "host"    : "mysql",
-          "password": "${ETHERPAD_DB_PASSWORD}",
-          "database": "${ETHERPAD_DB_NAME}"
-        },
+cat << EOF > settings.json
+{
+  "title": "${ETHERPAD_TITLE}",
+  "ip": "0.0.0.0",
+  "port" :${ETHERPAD_PORT},
+  "sessionKey" : "${ETHERPAD_SESSION_KEY}",
+  "dbType" : "mysql",
+  "dbSettings" : {
+    "user"    : "${ETHERPAD_DB_USER}",
+    "host"    : "db",
+    "password": "${ETHERPAD_DB_PASSWORD}",
+    "database": "${ETHERPAD_DB_NAME}"
+  },
 EOF
 
   if [ $ETHERPAD_ADMIN_PASSWORD ]; then
@@ -65,17 +69,17 @@ EOF
     : ${ETHERPAD_ADMIN_USER:=admin}
 
 cat <<- EOF >> settings.json
-      "users": {
-        "${ETHERPAD_ADMIN_USER}": {
-          "password": "${ETHERPAD_ADMIN_PASSWORD}",
-          "is_admin": true
-        }
-      },
+  "users": {
+    "${ETHERPAD_ADMIN_USER}": {
+      "password": "${ETHERPAD_ADMIN_PASSWORD}",
+      "is_admin": true
+    }
+  },
 EOF
-  fi
+fi
 
 cat <<- EOF >> settings.json
-  }
+}
 EOF
 fi
 
